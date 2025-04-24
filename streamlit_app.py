d4f469320ac32547eeec17408916302e626bb791
almirajuneeta <almira.juneeta09@gmail.com>
2025-04-24 13:42:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 hello hi")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("views/IMG_0913.jpeg")
